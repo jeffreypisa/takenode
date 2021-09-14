6194966b8823a12534af4c20a54107185c0efac0
Jeffrey Pisa <jeffreypisa@macbook-pro-van-jeffrey.local>
2021-09-14 16:13:19 +0000

    Solution for menu on mobile

--- parts/header.php
+++ parts/header.php
@@ -4,8 +4,18 @@
 	</div>
 	<nav class="mod-nav">
 		<div class="btn-group" role="group" aria-label="Button group with nested dropdown">
-			<a href="info.php" class="btn btn-light <?php if ($activepage === 'info') { ?>active<?php } ?>">Info</a>
-			<a href="spreadtheword.php" class="btn btn-light <?php if ($activepage === 'spreadtheword') { ?>active<?php } ?>">Spread the Word</a>
+			<a href="info.php" class="btn btn-light <?php if ($activepage === 'info') { ?>active<?php } ?>">
+				<i class="fal fa-search d-md-none"></i>
+				<span class="d-none d-md-block">Search</span>
+			</a>
+			<a href="info.php" class="btn btn-light <?php if ($activepage === 'info') { ?>active<?php } ?>">
+				<i class="fal fa-info-circle d-md-none"></i>
+				<span class="d-none d-md-block">Info</span>
+			</a>
+			<a href="spreadtheword.php" class="btn btn-light <?php if ($activepage === 'spreadtheword') { ?>active<?php } ?>">
+				<i class="fal fa-share d-md-none"></i>
+				<span class="d-none d-md-block">Spread the Word</span>
+			</a>
 			
 			<div class="btn-group" role="group">
 				<button id="btnGroupDrop1" type="button" class="btn btn-light dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
